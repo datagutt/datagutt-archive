aed5a0954e33931132639d75d4e335a2d3ae9e51
Thomas Lekanger <datagutt@lekanger.no>
2015-01-05 21:44:19 +0100

    llamas

--- js/datagutt.js
+++ js/datagutt.js
@@ -1,21 +1,31 @@
 window.datagutt = {
   init: function(){
+    var self = this;
     var doc = document.documentElement;
     var avatar = document.getElementsByClassName('avatar')[0];
+    var clickCount = 0;
+    avatar.onclick = function(){
+      if(clickCount >= 3){
+        self.pizza();
+        clickCount = 0;
+      }
+    };
     if(avatar && avatar.className){
       avatar.style.visibility = 'visible';
       avatar.animate('bounceIn');
     }
-    this.pizza();
+    cheet('↑ ↑ ↓ ↓ ← → ← → b a', function (){
+      self.pizza();
+    });
   },
   pizza: function(){
-  	cheet('↑ ↑ ↓ ↓ ← → ← → b a', function (){
-		var doc = document.documentElement;
-    	var homeBackgroundOverlay = document.getElementById('home-background-overlay')[0];
-    	if(doc){
-    		doc.className += ' pizza';
-    		homeBackgroundOverlay.innerHTML = '<marquee><img src="./img/flames.gif"></marquee>';
-    	}
-  	});
+      var doc = document.documentElement;
+      var homeBackgroundOverlay = document.getElementById('home-background-overlay');
+      if(doc){
+        doc.className += ' pizza';
+        for(var i = 0; i < 50; i++){
+          homeBackgroundOverlay.innerHTML += '<marquee><img src="./img/lama.gif"></marquee>';
+        }
+      }
   }
 };
